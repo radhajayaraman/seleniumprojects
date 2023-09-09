1628ea4217e4c1009a36fb3d67d3576ca549efb3
91967 <91967@DESKTOP-2F5EMT8>
2023-09-09 21:16:36 +0530

    first time code push

---
+++ Github/src/Githubpractice/Day03.java
@@ -0,0 +1,14 @@
+package Githubpractice;
+
+public class Day03 {
+	
+	public void method1()
+	{
+		System.out.println("This is my first program");
+	}
+public static void main(String[] args) {
+	Day03 obj = new Day03();
+	obj.method1();
+	System.out.println("Method called");
+}
+}
